d5a5784007bc1fdcc0c353f60857d37ad294da8f
czc13611858691@gmail.com <czc13611858691@gmail.com>
2023-09-16 11:04:27 +0800

    增加版本信息

--- source/conf.py
+++ source/conf.py
@@ -36,4 +36,4 @@ html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_static_path = ['_static']
 
-# html_search_options = {'dict': 'C:/Users/Admin/AppData/Local/Programs/Python/Python37/Lib/site-packages/jieba/dict.txt'}
+html_search_options = {'dict': 'C:/Users/Admin/AppData/Local/Programs/Python/Python37/Lib/site-packages/jieba/dict.txt'}
